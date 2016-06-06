4dfd39c2b6845c2d1d5e8d662730a3ca0ad978a7
Stephen Ray <SR2610Email@Gmail.com>
2016-06-06 19:45:13 +0100

    Added update checker

--- CreeperConfetti/src/main/java/com/sr2610/creeperconfetti/ConfettiMod.java
+++ CreeperConfetti/src/main/java/com/sr2610/creeperconfetti/ConfettiMod.java
@@ -7,12 +7,13 @@ import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ConfettiMod.MODID, version = ConfettiMod.VERSION)
+@Mod(modid = ConfettiMod.MODID, version = ConfettiMod.VERSION, updateJSON = ConfettiMod.UPDATEJSON)
 public class ConfettiMod {
 	public static final String MODID = "creeperconfetti";
-	public static final String VERSION = "1.1";
+	public static final String VERSION = "1.1.1";
 	public static final String PROXY_COMMON = "com.sr2610.creeperconfetti.proxy.CommonProxy";
 	public static final String PROXY_CLIENT = "com.sr2610.creeperconfetti.proxy.ClientProxy";
+	public static final String UPDATEJSON = "http://sr2610.com/micromods/confetti/confettiUpdate.json";
 
 	@SidedProxy(serverSide = PROXY_COMMON, clientSide = PROXY_CLIENT)
 	public static CommonProxy proxy;
