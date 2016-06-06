2f0f5b54b897f96401df015856f18175d10068d7
Stephen Ray <SR2610Email@Gmail.com>
2016-06-06 23:28:04 +0100

    Start work on Death Chests mod.

---
+++ DeathChests/src/main/java/com/sr2610/deathchests/DeathChests.java
@@ -0,0 +1,15 @@
+package com.sr2610.deathchests;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = DeathChests.MODID, version = DeathChests.VERSION)
+public class DeathChests {
+	public static final String MODID = "deathchests";
+	public static final String VERSION = "1.0";
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+	}
+}
