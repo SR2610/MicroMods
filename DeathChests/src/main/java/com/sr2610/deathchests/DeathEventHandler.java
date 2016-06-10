ace2a646d945c08aee26ae24a1f197ff642f5880
Stephen Ray <SR2610Email@Gmail.com>
2016-06-10 14:15:45 +0100

    Added Chest Filling Functionality

--- DeathChests/src/main/java/com/sr2610/deathchests/DeathEventHandler.java
+++ DeathChests/src/main/java/com/sr2610/deathchests/DeathEventHandler.java
@@ -3,8 +3,11 @@ package com.sr2610.deathchests;
 import java.util.List;
 
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
+import net.minecraft.tileentity.TileEntityChest;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.event.entity.player.PlayerDropsEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
@@ -13,7 +16,31 @@ public class DeathEventHandler {
 	@SubscribeEvent
 	public void handlePlayerDeath(PlayerDropsEvent event) {
 		if (checkForChest(event.getDrops()))
-			System.out.println("Found a chest");
+			transferIntoChest(event);
+
+	}
+
+	private void transferIntoChest(PlayerDropsEvent event) {
+		int stacksTransferred = 0;
+		boolean isFull = false;
+		EntityPlayer player = event.getEntityPlayer();
+		BlockPos position = new BlockPos(player.posX, player.posY, player.posZ);
+		player.worldObj.setBlockState(position, Blocks.CHEST.getDefaultState(), 2);
+		TileEntityChest chest = (TileEntityChest) player.worldObj.getTileEntity(position);
+
+		for (EntityItem droppedStack : event.getDrops()) {
+			if (!isFull) {
+				if(droppedStack.getEntityItem().stackSize==0)
+					droppedStack.setDead();
+				else{
+				chest.setInventorySlotContents(stacksTransferred, droppedStack.getEntityItem());
+				droppedStack.setDead();
+				stacksTransferred++;
+				if (stacksTransferred > chest.getSizeInventory())
+					isFull = true;
+			}
+			}
+		}
 
 	}
 
@@ -21,6 +48,7 @@ public class DeathEventHandler {
 
 		for (EntityItem droppedStack : list) {
 			if (droppedStack.getEntityItem().getItem() == Item.getItemFromBlock(Blocks.CHEST)) {
+				droppedStack.getEntityItem().stackSize --;
 				return true;
 			}
 		}
