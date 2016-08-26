22d76d89a2c1911d16c97832bd084681d7803993
Stephen Ray <SR2610Email@Gmail.com>
2016-08-26 17:36:48 +0100

    Updated Creeper Confetti to 1.10.2

--- CreeperConfetti/src/main/java/com/sr2610/creeperconfetti/ConfettiMod.java
+++ CreeperConfetti/src/main/java/com/sr2610/creeperconfetti/ConfettiMod.java
@@ -11,7 +11,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = ConfettiMod.MODID, version = ConfettiMod.VERSION, updateJSON = ConfettiMod.UPDATEJSON, guiFactory = ConfettiMod.GUIFACTORY)
 public class ConfettiMod {
 	public static final String MODID = "creeperconfetti";
-	public static final String VERSION = "1.2";
+	public static final String VERSION = "1.3";
 	public static final String PROXY_COMMON = "com.sr2610.creeperconfetti.proxy.CommonProxy";
 	public static final String PROXY_CLIENT = "com.sr2610.creeperconfetti.proxy.ClientProxy";
 	public static final String GUIFACTORY = "com.sr2610.creeperconfetti.config.GuiFactory";
