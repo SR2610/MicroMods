6f6a4e1f8e5a0d06f0af31df8cca7f337efcff1e
Stephen Ray <SR2610Email@Gmail.com>
2017-04-07 21:56:01 +0100

    Updated in code and Build

--- CreeperConfetti/src/main/java/com/sr2610/creeperconfetti/ConfettiMod.java
+++ CreeperConfetti/src/main/java/com/sr2610/creeperconfetti/ConfettiMod.java
@@ -15,7 +15,7 @@ public class ConfettiMod {
 	public static final String PROXY_COMMON = "com.sr2610.creeperconfetti.proxy.CommonProxy";
 	public static final String PROXY_CLIENT = "com.sr2610.creeperconfetti.proxy.ClientProxy";
 	public static final String GUIFACTORY = "com.sr2610.creeperconfetti.config.GuiFactory";
-	public static final String UPDATEJSON = "http://sr2610.com/micromods/confetti/confettiUpdate.json";
+	public static final String UPDATEJSON = "https://raw.githubusercontent.com/SR2610/MicroMods/master/CreeperConfetti/confettiUpdate.json";
 
 	@SidedProxy(serverSide = PROXY_COMMON, clientSide = PROXY_CLIENT)
 	public static CommonProxy proxy;
