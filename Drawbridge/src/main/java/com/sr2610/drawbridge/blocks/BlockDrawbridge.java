9f30a67a1ac15d2c8b92c11b76e7c317bd70d273
Stephen Ray <SR2610Email@Gmail.com>
2017-06-29 01:42:42 +0100

    Last Commit for the Night

--- Drawbridge/src/main/java/com/sr2610/drawbridge/blocks/BlockDrawbridge.java
+++ Drawbridge/src/main/java/com/sr2610/drawbridge/blocks/BlockDrawbridge.java
@@ -1,14 +1,32 @@
 package com.sr2610.drawbridge.blocks;
 
+import java.util.Random;
+
 import com.sr2610.drawbridge.DrawbridgeMod;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockDirectional;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.Mirror;
+import net.minecraft.util.Rotation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class BlockDrawbridge extends Block {
+
+	public static final PropertyDirection FACING = BlockDirectional.FACING;
+	public static final PropertyBool TRIGGERED = PropertyBool.create("triggered");
 
 	public BlockDrawbridge(String unlocalizedName) {
 		super(Material.IRON, MapColor.IRON);
@@ -16,7 +34,101 @@ public class BlockDrawbridge extends Block {
 		setCreativeTab(CreativeTabs.REDSTONE);
 		setHardness(3.5F);
 		setSoundType(SoundType.METAL);
+		setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH).withProperty(TRIGGERED,
+				Boolean.valueOf(false)));
 
+	}
+
+	public int tickRate(World worldIn) {
+		return 4;
+	}
+
+	public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
+		if (!worldIn.isRemote) {
+		}
+	}
+
+	public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state) {
+		super.onBlockAdded(worldIn, pos, state);
+		this.setDefaultDirection(worldIn, pos, state);
+	}
+
+	private void setDefaultDirection(World worldIn, BlockPos pos, IBlockState state) {
+		if (!worldIn.isRemote) {
+			EnumFacing enumfacing = (EnumFacing) state.getValue(FACING);
+			boolean flag = worldIn.getBlockState(pos.north()).isFullBlock();
+			boolean flag1 = worldIn.getBlockState(pos.south()).isFullBlock();
+
+			if (enumfacing == EnumFacing.NORTH && flag && !flag1) {
+				enumfacing = EnumFacing.SOUTH;
+			} else if (enumfacing == EnumFacing.SOUTH && flag1 && !flag) {
+				enumfacing = EnumFacing.NORTH;
+			} else {
+				boolean flag2 = worldIn.getBlockState(pos.west()).isFullBlock();
+				boolean flag3 = worldIn.getBlockState(pos.east()).isFullBlock();
+
+				if (enumfacing == EnumFacing.WEST && flag2 && !flag3) {
+					enumfacing = EnumFacing.EAST;
+				} else if (enumfacing == EnumFacing.EAST && flag3 && !flag2) {
+					enumfacing = EnumFacing.WEST;
+				}
+			}
+
+			worldIn.setBlockState(pos,
+					state.withProperty(FACING, enumfacing).withProperty(TRIGGERED, Boolean.valueOf(false)), 2);
+		}
+	}
+
+	public void neighborChanged(IBlockState state, World worldIn, BlockPos pos, Block blockIn, BlockPos fromPos) {
+		boolean flag = worldIn.isBlockPowered(pos) || worldIn.isBlockPowered(pos.up());
+		boolean flag1 = ((Boolean) state.getValue(TRIGGERED)).booleanValue();
+
+		if (flag && !flag1) {
+			worldIn.scheduleUpdate(pos, this, this.tickRate(worldIn));
+			worldIn.setBlockState(pos, state.withProperty(TRIGGERED, Boolean.valueOf(true)), 4);
+		} else if (!flag && flag1) {
+			worldIn.setBlockState(pos, state.withProperty(TRIGGERED, Boolean.valueOf(false)), 4);
+		}
+	}
+
+	public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY,
+			float hitZ, int meta, EntityLivingBase placer) {
+		return this.getDefaultState().withProperty(FACING, EnumFacing.getDirectionFromEntityLiving(pos, placer))
+				.withProperty(TRIGGERED, Boolean.valueOf(false));
+	}
+
+	public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer,
+			ItemStack stack) {
+		worldIn.setBlockState(pos, state.withProperty(FACING, EnumFacing.getDirectionFromEntityLiving(pos, placer)), 2);
+
+	}
+
+	public IBlockState getStateFromMeta(int meta) {
+		return this.getDefaultState().withProperty(FACING, EnumFacing.getFront(meta & 7)).withProperty(TRIGGERED,
+				Boolean.valueOf((meta & 8) > 0));
+	}
+
+	public int getMetaFromState(IBlockState state) {
+		int i = 0;
+		i = i | ((EnumFacing) state.getValue(FACING)).getIndex();
+
+		if (((Boolean) state.getValue(TRIGGERED)).booleanValue()) {
+			i |= 8;
+		}
+
+		return i;
+	}
+
+	public IBlockState withRotation(IBlockState state, Rotation rot) {
+		return state.withProperty(FACING, rot.rotate((EnumFacing) state.getValue(FACING)));
+	}
+
+	public IBlockState withMirror(IBlockState state, Mirror mirrorIn) {
+		return state.withRotation(mirrorIn.toRotation((EnumFacing) state.getValue(FACING)));
+	}
+
+	protected BlockStateContainer createBlockState() {
+		return new BlockStateContainer(this, new IProperty[] { FACING, TRIGGERED });
 	}
 
 }
